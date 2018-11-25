d2f7797e8d7c03c2c1cc3a5506f80973c00f85ec
Pedro Carvalho <43827142+pedrovrc@users.noreply.github.com>
2018-11-25 19:59:27 -0200

    Criação das texturas arrumadas
    Agora há remapeamento da escala do Cb e do Cr antes de colocá-los nos coeficientes da transformada.

--- bgr_to_textured.py
+++ bgr_to_textured.py
@@ -8,18 +8,13 @@ import matplotlib.pyplot as plt
 def bgr2ycbcr(image):  # Converts image from bgr space to ycbcr space
     height, width, channels = image.shape
 
-    Y = np.zeros((height, width), dtype=np.uint8)
-    Y_double = np.zeros((height, width), dtype=np.double)
+    Y = np.zeros((height, width), dtype=np.float64)
+    Y[:, :] += image[:, :, 2] * 0.299
+    Y[:, :] += image[:, :, 1] * 0.587
+    Y[:, :] += image[:, :, 0] * 0.114
 
-    # Y_double made to facilitate fast processing while avoiding rounding errors
-    Y_double[:, :] += image[:, :, 2] * 0.299
-    Y_double[:, :] += image[:, :, 1] * 0.587
-    Y_double[:, :] += image[:, :, 0] * 0.114
-
-    Y[:, :] = Y_double[:, :]
-
-    Cb = np.zeros((height, width), dtype=np.uint8)
-    Cr = np.zeros((height, width), dtype=np.uint8)
+    Cb = np.zeros((height, width), dtype=np.float64)
+    Cr = np.zeros((height, width), dtype=np.float64)
 
     # Other channels derived from Y channel, blue and red
     Cb[:, :] = (0.564 * image[:, :, 0]) - (0.564 * Y[:, :]) + 128
@@ -27,22 +22,20 @@ def bgr2ycbcr(image):  # Converts image from bgr space to ycbcr space
 
     return [Y, Cb, Cr]
 
-def remap_tonescale(image):
+
+def remap_tonescale(image, high, low):  # Used to stretch Cb and Cr mappings to fit cH and cV scales
     height, width = image.shape
 
     norm = np.zeros((height, width), dtype=np.float64)
-    cv2.normalize(image, norm, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_64F)
+    cv2.normalize(image, norm, alpha=low, beta=high, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_64F)
 
-    scaled = np.zeros((height, width), dtype=np.uint8)
-    scaled[:, :] = norm[:, :] * 255
-
-    return scaled
+    return norm
 # ---------- End of Functions ----------
 
 
 # ---------- Start of Main Program ----------
 # Load and show original bgr image
-bgr_image = cv2.imread("lena.jpg")
+bgr_image = cv2.imread("mapa.png")
 height, width, channels = bgr_image.shape
 cv2.imshow("original", bgr_image)
 cv2.waitKey(0)
@@ -53,8 +46,8 @@ cv2.imshow("Y", Y_channel)
 cv2.imshow("Cb", Cb_channel)
 cv2.imshow("Cr", Cr_channel)
 cv2.waitKey(0)
-# cv2.imwrite("original_Cb.png", Cb_channel)
-# cv2.imwrite("original_Cr.png", Cr_channel)
+cv2.imwrite("original_Cb.png", Cb_channel)
+cv2.imwrite("original_Cr.png", Cr_channel)
 
 # Compute wavelet transform
 coeffs = pywt.dwt2(Y_channel, 'haar')
@@ -66,26 +59,20 @@ titles = ['Approximation', ' Horizontal detail',
 fig = plt.figure(figsize=(12, 3))
 for i, a in enumerate([cA, cH, cV, cD]):
     ax = fig.add_subplot(1, 4, i + 1)
-    ax.imshow(a, interpolation="nearest", cmap=plt.cm.gray)
+    ax.imshow(a, interpolation="bicubic", cmap=plt.cm.gray)
     ax.set_title(titles[i], fontsize=10)
     ax.set_xticks([])
     ax.set_yticks([])
 fig.tight_layout()
 plt.show()
 
-# Replace cH and cV with Cb and Cr channels, respectively
-new_Cb = cv2.resize(Cb_channel, (cH.shape[1], cH.shape[0]))
-new_Cr = cv2.resize(Cr_channel, (cV.shape[1], cV.shape[0]))
-cH[:, :] = new_Cb[:, :]
-cV[:, :] = new_Cr[:, :]
+# Remap Cb and Cr channels to fit cH and cV scales
+Cb_channel = remap_tonescale(Cb_channel, cH.max(), cH.min())
+Cr_channel = remap_tonescale(Cr_channel, cV.max(), cV.min())
 
-#
-#
-#
-# Try putting max and min values in cH and cV
-#
-#
-#
+# Replace cH and cV with Cb and Cr channels, respectively
+cH = cv2.resize(Cb_channel, (cH.shape[1], cH.shape[0]))
+cV = cv2.resize(Cr_channel, (cV.shape[1], cV.shape[0]))
 
 # Show components of wavelet transform after inclusion of Cb and Cr
 titles = ['Approximation', ' Horizontal detail',
@@ -93,7 +80,7 @@ titles = ['Approximation', ' Horizontal detail',
 fig = plt.figure(figsize=(12, 3))
 for i, a in enumerate([cA, cH, cV, cD]):
     ax = fig.add_subplot(1, 4, i + 1)
-    ax.imshow(a, interpolation="nearest", cmap=plt.cm.gray)
+    ax.imshow(a, interpolation="bicubic", cmap=plt.cm.gray)
     ax.set_title(titles[i], fontsize=10)
     ax.set_xticks([])
     ax.set_yticks([])
@@ -101,8 +88,8 @@ fig.tight_layout()
 plt.show()
 
 # Compute inverse transform and show result
-result = pywt.idwt2((cA, (new_Cb, new_Cr, cD)), 'haar')
-plt.imshow(result, interpolation="nearest", cmap=plt.cm.gray)
+result = pywt.idwt2((cA, (cH, cV, cD)), 'haar')
+plt.imshow(result, interpolation="bicubic", cmap=plt.cm.gray)
 plt.show()
 
 # Save result image
